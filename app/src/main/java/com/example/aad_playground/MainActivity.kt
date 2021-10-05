d1493ab114f472061390f47801e5463f84a733b6
Javier_Man <javiermacemu@gmail.com>
2021-10-05 20:40:52 +0200

    Creada clase ManageFiles con los métodos createFile y listFile

--- app/src/main/java/com/example/aad_playground/MainActivity.kt
+++ app/src/main/java/com/example/aad_playground/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.aad_playground
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.example.aad_playground.ut_01.DataStorageType
 import com.example.aad_playground.ut_01.FilePlayGround
+import com.example.aad_playground.ut_01.ManageFiles
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var inputNameFile : AppCompatEditText
+    lateinit var inputContentFile : AppCompatEditText
+    lateinit var actionSave : AppCompatButton
+    lateinit var actionExplorer : AppCompatButton
+    lateinit var viewerFiles : TextView
 
-    val colors : MutableList<String> = mutableListOf()
+
+    //val colors : MutableList<String> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +35,39 @@ class MainActivity : AppCompatActivity() {
         readColors(lst)
         */
 
-        val dataStorageType = DataStorageType(this)
+        //val dataStorageType = DataStorageType(this)
         //dataStorageType.privateFile()
         //dataStorageType.privateFileCache()
         //dataStorageType.privateExternalFile()
         //dataStorageType.privatExternalCacheFile()
 
+        val manageFiles = ManageFiles(this)
+
+        inputNameFile = findViewById(R.id.input_name_file)
+        inputContentFile = findViewById(R.id.input_content_file)
+        actionSave = findViewById(R.id.action_save)
+        actionSave.setOnClickListener {
+            manageFiles.createFile(inputNameFile.text.toString(), inputContentFile.text.toString())
+        }
+        viewerFiles = findViewById(R.id.action_explorer)
+        actionExplorer = findViewById(R.id.action_explorer)
+
     }
 
+    /**
+     * Incompleto
+     */
+    private fun readFiles(files : MutableList<String>){
+
+        Thread (Runnable {
+            files.forEach {
+                runOnUiThread{
+                    //viewerFiles.setText(viewerFiles.text.toString() + files.get(it) + "\n")
+                }
+            }
+        })
+    }
+    /*
     private fun initColors(){
         colors.add("Blue")
         colors.add("Red")
@@ -45,4 +83,6 @@ class MainActivity : AppCompatActivity() {
             Log.d("@dev", it)
         }
     }
+    */
+
 }
---
+++ app/src/main/java/com/example/aad_playground/ut_01/ManageFiles.kt
@@ -0,0 +1,24 @@
+package com.example.aad_playground.ut_01
+
+import android.text.Editable
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+class ManageFiles (private val activity: AppCompatActivity){
+
+    fun createFile(fileName : String, fileText : String){
+        if (fileName.isEmpty().not() || fileText.isEmpty().not()){
+            val file = File(activity.filesDir, fileName)
+            file.writeText(fileText)
+        }
+    }
+
+    fun listFile() : MutableList<String> {
+        val filesArrayList = activity.filesDir.list()
+        return if (filesArrayList.isEmpty().not()){
+            filesArrayList.toMutableList()
+        }else{
+            mutableListOf()
+        }
+    }
+}
