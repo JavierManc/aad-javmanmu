ba360ff4c78c72df2d854e0b2429e9a2df5a64ee
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:40:20 +0100

    Creada Entidad PersonEntity con el método toModel que la convierte a PersonModel

---
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/data/Entities.kt
@@ -0,0 +1,16 @@
+package com.example.aad_playground.ut03.ex02.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.aad_playground.ut03.ex02.domain.PersonModel
+
+@Entity(tableName = "person")
+data class PersonEntity(
+    @PrimaryKey @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "age") val age: Int
+){
+
+    fun toModel(): PersonModel = PersonModel(id, name, age, null)
+}
