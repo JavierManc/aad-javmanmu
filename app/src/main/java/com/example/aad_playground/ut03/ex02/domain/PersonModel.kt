c0efaeb0ff4d717e28ef67df79d977afeb177df6
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:12:36 +0100

    Creadas clases PersonModel y PetModel

--- app/src/main/java/com/example/aad_playground/ut03/ex02/domain/PersonModel.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/domain/PersonModel.kt
@@ -1,4 +1,10 @@
 package com.example.aad_playground.ut03.ex02.domain
 
-class PersonModel {
-}
+data class PersonModel(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val address: String?
+)
+
+data class PetModel(val id: Int, val name: String, val age: Int)
