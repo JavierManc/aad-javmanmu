bd3a521aeb605805da126c08ba64b30df50827c5
Javier_Man <javiermacemu@gmail.com>
2021-11-09 20:03:18 +0100

    Añadida entidad PetEntity

--- app/src/main/java/com/example/aad_playground/ut03/ex02/data/Entities.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/data/Entities.kt
@@ -4,15 +4,17 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.aad_playground.ut03.ex02.domain.PersonModel
+import com.example.aad_playground.ut03.ex02.domain.PetModel
 
 @Entity(tableName = "person")
 data class PersonEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "age") val age: Int
+    @ColumnInfo(name = "age") val age: Int,
+    @ColumnInfo(name = "pet") val pet: PetEntity
 ) {
 
-    fun toModel(): PersonModel = PersonModel(id, name, age, null)
+    fun toModel(): PersonModel = PersonModel(id, name, age, null, pet.toModel())
 }
 
 @Entity(tableName = "pet")
@@ -20,4 +22,6 @@ data class PetEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "age") val age: Int
-)
+) {
+    fun toModel(): PetModel = PetModel(id, name, age)
+}
