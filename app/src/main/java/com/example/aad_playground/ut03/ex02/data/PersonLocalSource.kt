98bdf56a1660317ea4342c369ae53a56965f4440
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:47:31 +0100

    Creada clase PersonLocalSource

--- app/src/main/java/com/example/aad_playground/ut03/ex02/data/PersonLocalSource.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/data/PersonLocalSource.kt
@@ -1,4 +1,34 @@
 package com.example.aad_playground.ut03.ex02.data
 
-class PersonLocalSource {
+import android.content.Context
+import com.example.aad_playground.ut03.ex02.app.Ut03Ex02DataBase
+import com.example.aad_playground.ut03.ex02.domain.PersonModel
+
+class PersonLocalSource(aplicationContext: Context) {
+
+    private val db = Ut03Ex02DataBase.getInstance(aplicationContext)
+
+    init {
+        //Workaround
+        Thread{
+            db.clearAllTables()
+            Thread.sleep(2000)
+        }.start()
+    }
+
+    fun findAll(): List<PersonModel> {
+        val people = db.personDao().findAll()
+        return people.map { peopleEntity -> peopleEntity.toModel() }
+    }
+
+    fun save(personalModel: PersonModel) {
+        db.personDao().insert(
+            PersonEntity(
+                personalModel.id,
+                personalModel.name,
+                personalModel.age
+            )
+        )
+    }
+
 }
