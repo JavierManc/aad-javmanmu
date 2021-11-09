7b9fec416dbdcb80d4df26a15b47fcbbaaed4bd6
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:48:47 +0100

    Creada actividad para el ejercicio

--- app/src/main/java/com/example/aad_playground/ut03/ex02/presentation/Example02Activity.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/presentation/Example02Activity.kt
@@ -2,11 +2,33 @@ package com.example.aad_playground.ut03.ex02.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.aad_playground.R
+import com.example.aad_playground.ut03.ex02.data.PersonDataRepository
+import com.example.aad_playground.ut03.ex02.data.PersonLocalSource
+import com.example.aad_playground.ut03.ex02.domain.PersonModel
+import com.example.aad_playground.ut03.ex02.domain.PersonRepository
 
 class Example02Activity : AppCompatActivity() {
+
+    private val TAG = Example02Activity::class.java.simpleName
+
+    private val repository: PersonRepository by lazy {
+        PersonDataRepository(PersonLocalSource(applicationContext))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example02)
+        executeQuery()
+    }
+
+    private fun executeQuery() {
+        Thread {
+            repository.savePerson(PersonModel(1, "Name01", 1, "1"))
+            val people = repository.fetchAll()
+            Log.d(TAG, "$people")
+        }.start()
     }
 }
