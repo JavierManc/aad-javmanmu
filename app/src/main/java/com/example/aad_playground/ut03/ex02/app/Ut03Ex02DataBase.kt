254efc3c96422f35487a84811ad1d5b338cdc53d
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:40:50 +0100

    Creada clase abstracta que extiende de RoomDatabase

---
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/app/Ut03Ex02DataBase.kt
@@ -0,0 +1,33 @@
+package com.example.aad_playground.ut03.ex02.app
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.aad_playground.ut03.ex02.data.PersonDao
+import com.example.aad_playground.ut03.ex02.data.PersonEntity
+
+@Database(entities = [PersonEntity::class], version = 1)
+abstract class Ut03Ex02DataBase : RoomDatabase() {
+    abstract fun personDao(): PersonDao
+
+    companion object {
+        @Volatile
+        private var instance: Ut03Ex02DataBase? = null
+
+        fun getInstance(applicationContext: Context): Ut03Ex02DataBase {
+            if (instance == null) {
+                instance = buildDataBase(applicationContext)
+            }
+            return instance as Ut03Ex02DataBase
+        }
+
+        private fun buildDataBase(applicationContext: Context): Ut03Ex02DataBase {
+            return Room.databaseBuilder(
+                applicationContext,
+                Ut03Ex02DataBase::class.java,
+                "db-ut03-ex02"
+            ).build()
+        }
+    }
+}
