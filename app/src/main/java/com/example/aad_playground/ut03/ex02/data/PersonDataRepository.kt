a6282b94e6735dc380c6213c93886033c4a46e0e
Javier_Man <javiermacemu@gmail.com>
2021-11-09 19:48:30 +0100

    Creada clase PersonDataRepository

--- app/src/main/java/com/example/aad_playground/ut03/ex02/data/PersonDataRepository.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex02/data/PersonDataRepository.kt
@@ -1,4 +1,13 @@
 package com.example.aad_playground.ut03.ex02.data
 
-class PersonDataRepository {
+import com.example.aad_playground.ut03.ex02.domain.PersonModel
+import com.example.aad_playground.ut03.ex02.domain.PersonRepository
+
+class PersonDataRepository(private val personLocalSource: PersonLocalSource) : PersonRepository {
+    override fun savePerson(personModel: PersonModel) {
+        personLocalSource.save(personModel)
+    }
+
+    override fun fetchAll(): List<PersonModel> = personLocalSource.findAll()
+
 }
