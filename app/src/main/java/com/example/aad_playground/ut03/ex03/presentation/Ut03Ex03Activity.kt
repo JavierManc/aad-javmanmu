81e018d47c8fe201ade6891355fcb3a999a070f5
Javier Mancebo <javiermacemu@gmail.com>
2021-11-14 17:56:57 +0100

    Actualizada actividad implementados métodos gelAllAlerts y getAlertById

--- app/src/main/java/com/example/aad_playground/ut03/ex03/presentation/Ut03Ex03Activity.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex03/presentation/Ut03Ex03Activity.kt
@@ -2,31 +2,57 @@ package com.example.aad_playground.ut03.ex03.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.util.Log
 import com.example.aad_playground.R
-import com.example.aad_playground.ut03.ex03.app.RetrofitApiClient
+import com.example.aad_playground.ut03.ex03.app.remote.RetrofitApiClient
 import com.example.aad_playground.ut03.ex03.data.AlertDataRepository
+import com.example.aad_playground.ut03.ex03.data.local.AlertLocalSource
 import com.example.aad_playground.ut03.ex03.data.remote.AlertRemoteSource
-import com.example.aad_playground.ut03.ex03.domain.AlertRepository
+import com.example.aad_playground.ut03.ex03.domain.FindAlertUseCase
+import com.example.aad_playground.ut03.ex03.domain.GetAlertsUseCase
 
 class Ut03Ex03Activity : AppCompatActivity() {
 
     private val TAG = Ut03Ex03Activity::class.java.simpleName
 
-    private val viewModel: Ut03Ex03ViewModel by viewModels()
-
+    private lateinit var viewModel: Ut03Ex03ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ut03_ex03)
+        viewModel = Ut03Ex03ViewModel(
+            GetAlertsUseCase(
+                AlertDataRepository(
+                    AlertRemoteSource(RetrofitApiClient()),
+                    AlertLocalSource(
+                        applicationContext
+                    )
+                )
+            ),
+            FindAlertUseCase(
+                AlertDataRepository(
+                    AlertRemoteSource(RetrofitApiClient()),
+                    AlertLocalSource(applicationContext)
+                )
+            )
+        )
+        getAllAlerts()
+        getAlertById()
     }
 
+
     private fun getAllAlerts() {
-        val alerts = viewModel.getAlerts()
+        Thread {
+            val alerts = viewModel.getAlerts()
+            alerts.forEach { element -> Log.d(TAG, element.toString()) }
+        }.start()
     }
 
     private fun getAlertById() {
-        val alertId = ""
-        val alerts = viewModel.findAlert(alertId)
+        val alertId = "1900673"
+        Thread {
+            val alert = viewModel.findAlert(alertId)
+            Log.d(TAG, alert.toString())
+        }.start()
     }
 }
