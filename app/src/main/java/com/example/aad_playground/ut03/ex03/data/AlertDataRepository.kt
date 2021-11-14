7e8e0a50e914d59292f18bffc5f89ae1470ec947
Javier Mancebo <javiermacemu@gmail.com>
2021-11-14 17:52:54 +0100

    Actualizado AlertDataRepository en el que si no encuentra datos en la base de datos, los pide a red y los inserta en ella

--- app/src/main/java/com/example/aad_playground/ut03/ex03/data/AlertDataRepository.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex03/data/AlertDataRepository.kt
@@ -1,18 +1,26 @@
 package com.example.aad_playground.ut03.ex03.data
 
+import com.example.aad_playground.ut03.ex03.data.local.AlertLocalSource
 import com.example.aad_playground.ut03.ex03.data.remote.AlertRemoteSource
 import com.example.aad_playground.ut03.ex03.domain.AlertModel
 import com.example.aad_playground.ut03.ex03.domain.AlertRepository
 
-class AlertDataRepository(private val remoteSource: AlertRemoteSource) : AlertRepository {
+class AlertDataRepository(
+    private val remoteSource: AlertRemoteSource,
+    private val localSource: AlertLocalSource
+) : AlertRepository {
 
     override fun fetchAll(): List<AlertModel> {
-        val alerts = remoteSource.getAlerts()
-        TODO("Not yet implemented")
+        return if (localSource.getAlerts().isNullOrEmpty()) {
+            val alerts = remoteSource.getAlerts()
+            alerts.map { element -> localSource.save(element) }
+            alerts
+        } else {
+            localSource.getAlerts()
+        }
     }
 
     override fun fetchById(alertId: String): AlertModel {
-        val alerts = remoteSource.getAlert(alertId)
-        TODO("Not yet implemented")
+        return localSource.getAlert(alertId)
     }
 }
