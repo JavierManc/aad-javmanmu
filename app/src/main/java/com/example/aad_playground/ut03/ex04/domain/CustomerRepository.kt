7c8030c1927ef1ebcbb4aec2bb590ba38832e093
Javier_Man <javiermacemu@gmail.com>
2021-11-30 19:01:43 +0100

    Agregada interfaz InvoiceRepository

--- app/src/main/java/com/example/aad_playground/ut03/ex04/domain/CustomerRepository.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex04/domain/CustomerRepository.kt
@@ -1,8 +1,15 @@
 package com.example.aad_playground.ut03.ex04.domain
 
-interface ModelRepository {
+interface CustomerRepository {
     fun saveCustomerList(modelList: List<CustomerModel>)
     fun saveCustomerModel(model: CustomerModel)
     fun fetchAllCustomers(): List<CustomerModel>
     fun fetchCustomerById(id: Int): CustomerModel
+}
+
+interface InvoiceRepository {
+    fun saveInvoiceList(modelList: List<InvoiceModel>)
+    fun saveInvoiceModel(model: InvoiceModel)
+    fun fetchAllInvoice(): List<InvoiceModel>
+    fun fetchInvoiceById(id: Int): InvoiceModel
 }
