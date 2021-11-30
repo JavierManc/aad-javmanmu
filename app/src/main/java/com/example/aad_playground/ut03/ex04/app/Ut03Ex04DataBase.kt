dbbd5e70b29c657b14d966042d5db22926158523
Javier_Man <javiermacemu@gmail.com>
2021-11-30 19:00:05 +0100

    Creada base de datos con dos Dao

--- app/src/main/java/com/example/aad_playground/ut03/ex04/app/Ut03Ex04DataBase.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex04/app/Ut03Ex04DataBase.kt
@@ -4,17 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.aad_playground.ut03.ex04.data.CustomerDao
-import com.example.aad_playground.ut03.ex04.data.CustomerEntity
+import com.example.aad_playground.ut03.ex04.data.*
 
 @Database(
-    entities = [CustomerEntity::class],
+    entities = [CustomerEntity::class, ProductEntity::class, InvoiceEntity::class, InvoiceLinesEntity::class],
     version = 1,
     exportSchema = false
 )
 abstract class Ut03Ex04DataBase : RoomDatabase() {
 
     abstract fun customerDao(): CustomerDao
+    abstract fun invoiceDao(): InvoiceDao
+    abstract fun productDao(): ProductDao
 
     companion object {
         @Volatile
