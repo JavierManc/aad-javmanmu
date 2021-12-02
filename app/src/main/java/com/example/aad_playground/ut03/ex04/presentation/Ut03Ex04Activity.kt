ce15df2d423dab9beb3ab291269c7025e323e604
Javier_Man <javiermacemu@gmail.com>
2021-12-02 18:33:05 +0100

    Creada actividad y viewmodel para probar las consultas y las inserciones a la base de datos

--- app/src/main/java/com/example/aad_playground/ut03/ex04/presentation/Ut03Ex04Activity.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex04/presentation/Ut03Ex04Activity.kt
@@ -1,13 +1,70 @@
 package com.example.aad_playground.ut03.ex04.presentation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.aad_playground.R
+import com.example.aad_playground.ut03.ex04.data.CustomerLocalRepository
+import com.example.aad_playground.ut03.ex04.data.CustomerLocalSource
+import com.example.aad_playground.ut03.ex04.data.InvoiceLocalRepository
+import com.example.aad_playground.ut03.ex04.data.InvoiceLocalSource
+import com.example.aad_playground.ut03.ex04.domain.*
+import java.time.LocalDate
 
 class Ut03Ex04Activity : AppCompatActivity() {
 
+    private lateinit var viewModel: Ut03Ex04ViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ut03_ex04)
+        viewModel = Ut03Ex04ViewModel(
+            FetchAllCustomersUseCase(CustomerLocalRepository(CustomerLocalSource(this.applicationContext))),
+            FetchCustomerByIdUseCase(CustomerLocalRepository(CustomerLocalSource(this.applicationContext))),
+            SaveCustomerUseCase(CustomerLocalRepository(CustomerLocalSource(this.applicationContext))),
+            FetchAllInvoiceUseCase(InvoiceLocalRepository(InvoiceLocalSource(this.applicationContext))),
+            FetchInvoiceByIdUseCase(InvoiceLocalRepository(InvoiceLocalSource(this.applicationContext))),
+            SaveInvoiceUseCase(InvoiceLocalRepository(InvoiceLocalSource(this.applicationContext)))
+        )
+        customers.forEach { model ->
+            viewModel.saveCustomer(model)
+        }
+        viewModel.fetchAllCustomers()
+
+        invoices.forEach { model->
+            viewModel.saveInvoice(model)
+        }
+        viewModel.fetchAllInvoice()
     }
+
+    private val customers: List<CustomerModel> = mutableListOf(
+        CustomerModel(6, "Name1", "Surname1"),
+        CustomerModel(7, "Name2", "Surname2"),
+        CustomerModel(8, "Name3", "Surname3"),
+        CustomerModel(9, "Name4", "Surname4"),
+        CustomerModel(10, "Name5", "Surname5"),
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val invoices: List<InvoiceModel> = mutableListOf(
+        InvoiceModel(
+            1, LocalDate.now(), CustomerModel(1, "Name1", "Surname1"),
+            mutableListOf(
+                InvoiceLinesModel(1, ProductModel(1, "Product1", "Model1", 1.1)),
+                InvoiceLinesModel(2, ProductModel(2, "Product2", "Model2", 2.2)),
+                InvoiceLinesModel(3, ProductModel(3, "Product3", "Model3", 3.3)),
+            )
+        ),
+        InvoiceModel(
+            2, LocalDate.now(), CustomerModel(2, "Name2", "Surname2"),
+            mutableListOf(
+                InvoiceLinesModel(4, ProductModel(4, "Product4", "Model4", 1.1)),
+                InvoiceLinesModel(5, ProductModel(5, "Product5", "Model5", 2.2)),
+                InvoiceLinesModel(6, ProductModel(6, "Product6", "Model6", 3.3)),
+            )
+        )
+    )
 }
--- app/src/main/java/com/example/aad_playground/ut03/ex04/presentation/Ut03Ex04ViewModel.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex04/presentation/Ut03Ex04ViewModel.kt
@@ -1,8 +1,63 @@
 package com.example.aad_playground.ut03.ex04.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.aad_playground.ut03.ex04.domain.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class Ut03Ex04ViewModel: ViewModel() {
+class Ut03Ex04ViewModel(
+    private val fetchAllCustomersUseCase: FetchAllCustomersUseCase,
+    private val fetchCustomerByIdUseCase: FetchCustomerByIdUseCase,
+    private val saveCustomerUseCase: SaveCustomerUseCase,
+    private val fetchAllInvoiceUseCase: FetchAllInvoiceUseCase,
+    private val fetchInvoiceByIdUseCase: FetchInvoiceByIdUseCase,
+    private val saveInvoiceUseCase: SaveInvoiceUseCase
+) : ViewModel() {
 
+    fun fetchAllCustomers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val customers = fetchAllCustomersUseCase.execute()
+            customers.forEach { model ->
+                Log.d("Customer", model.toString())
+            }
+        }
 
+    }
+
+    fun fetchCustomerById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val customer = fetchCustomerByIdUseCase.execute(id)
+            Log.d("Customer", customer.toString())
+        }
+    }
+
+    fun saveCustomer(customer: CustomerModel) {
+        viewModelScope.launch(Dispatchers.IO) {
+            saveCustomerUseCase.execute(customer)
+        }
+    }
+
+    fun fetchAllInvoice() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val invoices = fetchAllInvoiceUseCase.execute()
+            invoices.forEach { model ->
+                Log.d("Invoice", model.toString())
+            }
+        }
+    }
+
+    fun fetchInvoiceById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val invoice = fetchInvoiceByIdUseCase.execute(id)
+            Log.d("Invoice", invoice.toString())
+        }
+    }
+
+    fun saveInvoice(invoice: InvoiceModel) {
+        viewModelScope.launch(Dispatchers.IO) {
+            saveInvoiceUseCase.execute(invoice)
+        }
+    }
 }
