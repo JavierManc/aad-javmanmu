9db55fb0b3edaf9902cc35dd08deafb7f4f08ca2
Javier_Man <javiermacemu@gmail.com>
2021-12-02 18:31:10 +0100

    Cambiada clase date a localdate

--- app/src/main/java/com/example/aad_playground/ut03/ex04/data/InvoiceLocalSource.kt
+++ app/src/main/java/com/example/aad_playground/ut03/ex04/data/InvoiceLocalSource.kt
@@ -1,27 +1,37 @@
 package com.example.aad_playground.ut03.ex04.data
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.aad_playground.ut03.ex04.app.Ut03Ex04DataBase
 import com.example.aad_playground.ut03.ex04.domain.InvoiceLinesModel
 import com.example.aad_playground.ut03.ex04.domain.InvoiceModel
 import com.example.aad_playground.ut03.ex04.domain.ProductModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class InvoiceLocalSource(aplicationContext: Context) {
 
     private val db = Ut03Ex04DataBase.getInstance(aplicationContext)
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun fetchAll(): List<InvoiceModel> {
         return db.invoiceDao().findAllInvoice().map { entity -> invoiceLinesToModel(entity) }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun fetchById(id: Int): InvoiceModel {
         return invoiceLinesToModel(db.invoiceDao().findInvoiceById(id))
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun invoiceLinesToModel(invoiceLinesWithCustomer: InvoiceLinesWithCustomer): InvoiceModel {
         return InvoiceModel(
             invoiceLinesWithCustomer.invoiceEntity.id,
-            invoiceLinesWithCustomer.invoiceEntity.date,
+            LocalDate.parse(
+                invoiceLinesWithCustomer.invoiceEntity.date,
+                DateTimeFormatter.ISO_DATE
+            ),
             invoiceLinesWithCustomer.customerEntity.toModel(),
             invoiceLinesWithCustomer.invoiceLinesEntity.map { entity ->
                 InvoiceLinesModel(
@@ -38,7 +48,7 @@ class InvoiceLocalSource(aplicationContext: Context) {
 
     fun saveInvoice(invoice: InvoiceModel) {
         db.invoiceDao().saveInvoice(
-            InvoiceEntity(invoice.id, invoice.date, invoice.customerModel.id),
+            InvoiceEntity(invoice.id, invoice.date.toString(), invoice.customerModel.id),
             CustomerEntity(
                 invoice.customerModel.id,
                 invoice.customerModel.name,
