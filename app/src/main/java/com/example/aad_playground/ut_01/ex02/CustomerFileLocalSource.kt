bff5941f200e253fe4cba0132d563c3079de4604
Javier_Man <javiermacemu@gmail.com>
2021-12-09 11:33:04 +0100

    Actualizado gestor de ficheros de los clientes en el que se crea el fichero dentro de la clase

--- app/src/main/java/com/example/aad_playground/ut_01/ex02/CustomerFileLocalSource.kt
+++ app/src/main/java/com/example/aad_playground/ut_01/ex02/CustomerFileLocalSource.kt
@@ -1,5 +1,6 @@
 package com.example.aad_playground.ut_01.ex02
 
+import android.content.Context
 import com.example.aad_playground.ut_01.ex02.serializer.JsonSerializer
 import java.io.File
 
@@ -8,26 +9,35 @@ import java.io.File
  */
 class CustomerFileLocalSource(
     private val gson: JsonSerializer,
-    private val file: File
+    private val context: Context
 ) {
 
+    private val customersFile: File by lazy {
+        buildFile()
+    }
 
     /**
      * Función que me permite guardar un cliente en un fichero.
      */
     fun save(customer: CustomerModel) {
-        file.appendText(gson.toJson(customer, CustomerModel::class.java) + System.lineSeparator())
+        val customerList = fetch().toMutableList()
+        customerList.add(customer)
+        save(customerList)
     }
 
     /**
      * Función que me permite guardar un listado de clientes en un fichero.
      */
     fun save(customers: List<CustomerModel>) {
-        if (file.exists()) {
-            deleteFile()
-        }
-        customers.forEach { customer ->
-            save(customer)
+        //Borra todos los clientes antes de guardar la lista
+        customersFile.writeText("")
+        customers.map { model ->
+            customersFile.appendText(
+                gson.toJson(
+                    model,
+                    CustomerModel::class.java
+                ) + System.lineSeparator()
+            )
         }
     }
 
@@ -66,20 +76,18 @@ class CustomerFileLocalSource(
      * Función que me permite obtener un listado de todos los clientes almacenados en un fichero.
      */
     fun fetch(): List<CustomerModel> {
-        return if (file.exists()) {
-            file.readLines().map { entity ->
-                gson.fromJson(entity, CustomerModel::class.java)
-            }
-        } else {
-            return emptyList()
+        val customers: MutableList<CustomerModel> = mutableListOf()
+        val lines = customersFile.readLines()
+        lines.map { model ->
+            val customerModel = gson.fromJson(model, CustomerModel::class.java)
+            customers.add(customerModel)
         }
-
-
+        return customers
     }
 
     fun findById(customerId: Int): CustomerModel {
         val customerList = fetch()
-        var customer: CustomerModel = CustomerModel(0, "", "")
+        var customer = CustomerModel(0, "", "")
         customerList.forEach { entity ->
             if (entity.id == customerId) {
                 customer = entity
@@ -89,8 +97,19 @@ class CustomerFileLocalSource(
     }
 
     fun deleteFile() {
-        if (file.exists()) {
-            file.delete()
-        }
+        customersFile.delete()
     }
+
+    private fun buildFile(): File {
+        val file = File(context.filesDir, CUSTOMER_FILENAME)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        return file
+    }
+
+    companion object {
+        const val CUSTOMER_FILENAME: String = "aad_ut01_ex02_customer.txt"
+    }
+
 }
